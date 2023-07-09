562e9150930b0d61d01c98ce71e4a3be3fbd2123
Jorge Gajardo <jl.gajardoduran@gmail.com>
2023-07-09 01:23:21 -0400

    Primer commiit - proyecto TwitterCard

---
+++ project/00-Hola-Mundo/src/App.jsx
@@ -0,0 +1,60 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
+function App() {
+  // FUNCION DE FORMATEO PARA USERNAME (PROPS)
+  const formatUserName = (userName) => `@${userName}`
+  // OBJETO CON LAS PROPS QUE RECIBEN LOS COMPONENTES HIJOS
+  const midudev = {isFollowing: true, userName: 'midudev', name: 'Miguel Angel Duran'}
+  // Renderizado de listas con .map()
+  const users = [
+    {
+      name: 'dualipa',
+      userName: 'dualipa',
+      isFollowing: true
+    },
+    {
+      name: 'Post Malonee',
+      userName: 'postmalone',
+      isFollowing: true
+    },
+    {
+      name: 'daltonico',
+      userName: 'dalto',
+      isFollowing: false
+    },
+    {
+      name: 'Desconcido',
+      isFollowing: false
+    }
+  ]
+  // RENDERIZACION
+  return (
+    <section className='App'>
+
+      <TwitterFollowCard 
+        {...midudev}
+        formatUserName={formatUserName}
+      />
+
+    {
+      users.map((user, index) => {
+        const {name, userName, isFollowing,} = user
+        return (
+          <TwitterFollowCard 
+            key={index}
+            userName={userName}
+            initialFollowing={isFollowing}
+            formatUserName={formatUserName}
+          >
+            {name}
+          </TwitterFollowCard>
+        )
+      })
+    }
+
+    </section>
+  )
+}
+
+export default App
---
+++ project/00-Hola-Mundo/src/main.jsx
@@ -0,0 +1,33 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+// RAIZ RECUPERADA DEL INDEX - HTML
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// METODO DE LA RAIZ PARA RENDERIZAR UN COMPONENTE APP
+root.render(
+  <App />
+)
+
+// React Library
+// componentes y elementos
+// Interaces
+// Creacion de ROOT y de ELEMENTO
+// JSX
+// Fragment
+// Vite - Enpaquetador
+// Estilos en react - main.css and App.css
+// Pasando Props de padres a hijos
+// Props - Funciones - Estados - Array - Elementos - Objeto (spread operator)
+// Inmutabilidad de los props
+// Prop - children
+// Evento onCLick - handleClick()
+// Renderizado condicional
+// Virtual DOM - re-renderizado de padres e hijos
+// Hook - useState - [1, 2] = useState(initialState)
+// Renderizado de listas con .map()
+
+
+
+
