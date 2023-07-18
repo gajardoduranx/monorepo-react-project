136ffb76272cb75af642b86a3440f0f8d6a90de2
Jorge Gajardo <jl.gajardoduran@gmail.com>
2023-07-18 12:52:05 -0400

    add test playwright

--- project/04. Prueba-tecnica-Trainee/services/facts.js
+++ project/04. Prueba-tecnica-Trainee/services/facts.js
@@ -1,10 +1,8 @@
 const CAT_ENDPOINT_RAMDOM_FACT = 'https://catfact.ninja/fact'
 
-export const getRandomFact = () => {
-  return fetch(CAT_ENDPOINT_RAMDOM_FACT)
-    .then((response) => response.json())
-    .then((data) => {
-      const { fact } = data
-      return fact
-    })
+export const getRandomFact = async () => {
+  const response = await fetch(CAT_ENDPOINT_RAMDOM_FACT)
+  const data = await response.json()
+  const { fact } = data
+  return fact
 }
